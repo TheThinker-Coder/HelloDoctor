39e48bfbeccaa413edc2defcfc6182592a8638c0
TheThinker-Coder <74201472+TheThinker-Coder@users.noreply.github.com>
2021-11-09 16:34:58 +0530

    Delete UsersService.java

--- src/main/java/com/hellodoctor/services/UsersService.java
+++
@@ -1,9 +0,0 @@
-package com.hellodoctor.services;
-
-import com.hellodoctor.entities.User;
-import com.hellodoctor.requestdto.SignInRequestDto;
-
-public interface UsersService {
-	public User  signIn(SignInRequestDto signInRequestDto);
-
-}
